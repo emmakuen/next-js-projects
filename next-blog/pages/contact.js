d02d0ed582255ff285ddb3a1676ebaba17f43eea
emmakuen <emmaku@protonmail.com>
2021-12-15 23:15:35 +0900

    add pages

---
+++ next-blog/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const Contact = () => {
+  return (
+    <div>
+      <Head>
+        <title>Contact - My Blog</title>
+      </Head>
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ next-blog/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Head>
+        <title>My Blog</title>
+        <meta name="description" value="This is my blog" />
+      </Head>
+      <h1>My Blog</h1>
+    </div>
+  );
+};
+
+export default HomePage;
