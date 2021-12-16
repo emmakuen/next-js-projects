32f2a92943b8ca1d1e95ec5ccf47384ae93339eb
emmakuen <emmaku@protonmail.com>
2021-12-16 22:58:29 +0900

    fetch posts dynamically

---
+++ next-blog/content/posts/third-post.md
@@ -0,0 +1,6 @@
+---
+date: "2021-12-17"
+title: "Third Post"
+---
+
+This is my third post, written in Markdown.
--- next-blog/lib/posts.js
+++ next-blog/lib/posts.js
@@ -1,4 +1,4 @@
-import { readFile } from "fs/promises";
+import { readdir, readFile } from "fs/promises";
 import matter from "gray-matter";
 import { marked } from "marked";
 
@@ -12,4 +12,12 @@ const getPost = async (slug) => {
   return { date, title, body };
 };
 
-export { getPost };
+const getSlugs = async () => {
+  const suffix = ".md";
+  const files = await readdir("content/posts");
+  return files
+    .filter((file) => file.endsWith(suffix))
+    .map((file) => file.slice(0, -suffix.length));
+};
+
+export { getPost, getSlugs };
