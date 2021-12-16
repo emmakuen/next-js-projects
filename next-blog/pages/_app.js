010aa17097a8d538aca31a92532c9e9c51ad84b3
emmakuen <emmaku@protonmail.com>
2021-12-16 13:14:44 +0900

    add public folder

--- next-blog/pages/_app.js
+++ next-blog/pages/_app.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import NavBar from "../components/NavBar";
 import "../styles/global.css";
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/icons/favicon.ico" />
+      </Head>
       <NavBar />
       <Component {...pageProps} />
     </>
