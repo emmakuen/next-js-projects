ac2ecf1affc6ec912a7fd59e20ce950482ca8b96
emmakuen <emmaku@protonmail.com>
2021-12-18 12:50:07 +0900

    add next-shop project

---
+++ next-shop/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="px-6 py-4">
+        <h1 className="text-2xl pb-4">Next Shop</h1>
+        <p>[TODO: display products]</p>
+      </main>
+    </>
+  );
+}
