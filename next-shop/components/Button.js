4be8cf72ebabc2b8c7f7833233974aee72019419
emmakuen <emmaku@protonmail.com>
2021-12-20 19:58:24 +0900

    style sign-in page

--- next-shop/components/Button.js
+++ next-shop/components/Button.js
@@ -4,7 +4,7 @@ const Button = ({ type, children }) => {
   return (
     <button
       type={type}
-      className="bg-green-600 my-2 hover:bg-green-500 text-white px-4 py-1 rounded"
+      className="bg-green-600 my-2 w-80 hover:bg-green-500 text-white px-4 py-1 rounded"
     >
       {children}
     </button>
--- next-shop/pages/sign-in.js
+++ next-shop/pages/sign-in.js
@@ -6,15 +6,20 @@ import Button from "../components/Button";
 const SignIn = () => {
   return (
     <Page title="Sign In">
-      <form>
-        <Field label="Email">
-          <Input type="email" />
-        </Field>
-        <Field label="password">
-          <Input type="password" />
-        </Field>
-        <Button type="submit">Sign In</Button>
-      </form>
+      <div className="lg:flex lg:justify-center">
+        <form>
+          <h1 className="text-sm text-gray-500 lg:text-center mb-4">
+            Welcome Back
+          </h1>
+          <Field label="Email">
+            <Input type="email" />
+          </Field>
+          <Field label="password">
+            <Input type="password" />
+          </Field>
+          <Button type="submit">Sign In</Button>
+        </form>
+      </div>
     </Page>
   );
 };
