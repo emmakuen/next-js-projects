2f114908b5411260b6f5d4db5e94f4c166030bb4
emmakuen <emmaku@protonmail.com>
2021-12-21 04:01:16 +0900

    add form state

--- next-shop/components/Input.js
+++ next-shop/components/Input.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Input = ({ type }) => {
-  return <input type={type} className="border rounded px-3 py-1 w-80" />;
+const Input = ({ type, value, handleChange, required }) => {
+  return (
+    <input
+      type={type}
+      value={value}
+      onChange={handleChange}
+      required={required}
+      className="border rounded px-3 py-1 w-80"
+    />
+  );
 };
 
 export default Input;
--- next-shop/pages/sign-in.js
+++ next-shop/pages/sign-in.js
@@ -1,21 +1,39 @@
+import { useState } from "react";
 import Page from "../components/Page";
 import Input from "../components/Input";
 import Field from "../components/Field";
 import Button from "../components/Button";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <Page title="Sign In">
       <div className="lg:flex lg:justify-center">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1 className="text-sm text-gray-500 lg:text-center mb-4">
             Welcome Back
           </h1>
           <Field label="Email">
-            <Input type="email" />
+            <Input
+              type="email"
+              value={email}
+              handleChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </Field>
           <Field label="password">
-            <Input type="password" />
+            <Input
+              type="password"
+              value={password}
+              handleChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </Field>
           <Button type="submit">Sign In</Button>
         </form>
