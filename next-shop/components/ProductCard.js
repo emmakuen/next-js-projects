9f81cfc6bcd97d9d1231e44cb34d0ab28874451a
emmakuen <emmaku@protonmail.com>
2021-12-20 16:07:25 +0900

    refactor images using next/image

--- next-shop/components/ProductCard.js
+++ next-shop/components/ProductCard.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const ProductCard = ({ product }) => {
   return (
     <div className="border w-80 shadow hover:shadow-xl">
       <Link href={`/products/${product.id}`}>
         <a>
-          <img src={product.pictureUrl} alt="product-image" />
+          <Image
+            src={product.pictureUrl}
+            alt="product-image"
+            width={320}
+            height={240}
+          />
           <div className="p-2 flex justify-between items-baseline">
             <h2 className="text-lg font-bold">{product.title}</h2>
             <span>${product.price}</span>
