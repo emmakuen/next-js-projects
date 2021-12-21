fe5111c7bda905259a19da7acbefd606b2102c87
emmakuen <emmaku@protonmail.com>
2021-12-21 16:13:28 +0900

    add styling to navbar

--- next-shop/components/NavBar.js
+++ next-shop/components/NavBar.js
@@ -2,15 +2,26 @@ import React from "react";
 import Link from "next/link";
 
 const NavBar = () => {
+  const user = { name: "Alice" };
   return (
     <nav className="px-2 py-1 text-sm lg:text-base">
-      <ul className="flex justify-between">
+      <ul className="flex gap-2">
         <li className="text-lg font-extrabold">
           <Link href="/">Next Shop</Link>
         </li>
-        <li>
-          <Link href="/sign-in">Sign In</Link>
-        </li>
+        <li role="separator" className="flex-1" />
+        {!user ? (
+          <li>
+            <Link href="/sign-in">Sign In</Link>
+          </li>
+        ) : (
+          <>
+            <li>{user.name}</li>
+            <li>
+              <button>Sign Out</button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
