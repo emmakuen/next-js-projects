c79981470d6249ae6f235e179fa8938c20d699e9
emmakuen <emmaku@protonmail.com>
2022-01-06 15:34:34 +0900

    add e-commerce project

---
+++ e-commerce-project/pages/index.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+import play from '../playground'
+
+export default function Home() {
+  
+  useEffect(() => {
+    play()
+  }, [])
+
+  return <div>Hello World</div>;
+}
