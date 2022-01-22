c3d308bbbca0f997db665da08afb9028c10ec2e1
emmakuen <emmaku@protonmail.com>
2022-01-23 05:04:44 +0900

    style footer

--- portfolio-app/components/Footer.js
+++ portfolio-app/components/Footer.js
@@ -1,7 +1,27 @@
-import React from "react";
+import { colors } from "../utils/colors";
 
 const Footer = () => {
-  return <footer>Copyright Emma</footer>;
+  return (
+    <>
+      <footer>
+        <small className="copyright-info">
+          &#169; Emmaku | All rights reserved.
+        </small>
+      </footer>
+      <style jsx>{`
+        footer {
+          display: flex;
+          justify-content: center;
+        }
+        .copyright-info {
+          position: fixed;
+          font-size: 14px;
+          color: ${colors.grey};
+          bottom: 2rem;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Footer;
